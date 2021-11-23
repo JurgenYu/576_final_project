97602a8bd21d06176a240a87c3a8d57915755769
xiaowusheryl123 <xiaowu@umass.edu>
2021-11-22 19:38:46 -0500

    modify the script and add the feature of rotate

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -7,39 +7,44 @@ using UnityEngine.UI;
 public class Car : MonoBehaviour
 {
     // Start is called before the first frame update、private Animator animation_controller;
-    private CharacterController character_controller;
+    //private CharacterController controller;
 	//private Animator animation_controller;
 	public GameObject car;
-    public Vector3 movement_direction;
-	
+    public Vector3 moveDirection;
+	public float turnSpeed = 10.0f;
     public float walking_velocity;   
-    public float velocity;
+    public float movementSpeed;
 	public float rotationSpeed = 10.0f;
+	public float gravity = 20.0f;	
     void Start()
     {
         //animation_controller = GetComponent<Animator>();
-        character_controller = GetComponent<CharacterController>();
-        movement_direction = new Vector3(0.0f, 0.0f, 0.0f);
+        //controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(0.0f, 0.0f, 0.0f);
         walking_velocity = 2.0f;
-        velocity = 0.0f;
+        movementSpeed = 5.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xdirection = Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
-        float zdirection = Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
-        movement_direction = new Vector3(xdirection, 0.0f, zdirection);
-		character_controller.Move(movement_direction * velocity * Time.deltaTime);
-		//if (character_controller.isGrounded){
-		if(Input.GetKey(KeyCode.UpArrow)){
-			velocity = 3.0f;
-		}else if(Input.GetKey(KeyCode.DownArrow)){
-			velocity = -3.0f;
-		}else if(Input.GetKeyUp(KeyCode.UpArrow)){
-			velocity = 0;
-		}else if(Input.GetKeyUp(KeyCode.DownArrow)){
-			velocity = 0;
-		}else{}
-    }
+        //float xdirection = Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
+        ////float zdirection = Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y);
+        //movement_direction = new Vector3(xdirection, 0.0f, zdirection);
+		//character_controller.Move(movement_direction * velocity * Time.deltaTime);
+		
+		if(Input.GetKey(KeyCode.UpArrow)) {
+             transform.position += transform.forward * Time.deltaTime * movementSpeed;
+         }
+         else if(Input.GetKey(KeyCode.DownArrow)) {
+             transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+         }else{}
+		 if(Input.GetKey(KeyCode.LeftArrow)) {
+             transform.Rotate(0, Time.deltaTime * turnSpeed, 0);
+         }
+         else if(Input.GetKey(KeyCode.RightArrow)) {
+             transform.Rotate(0, Time.deltaTime * (-turnSpeed), 0);
+         }
+	}
 }
+
