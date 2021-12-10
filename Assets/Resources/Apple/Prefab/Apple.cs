3c38536af585de5e195d636cdc44dbbef9067f5f
jurgen <tigeryhw@gmail.com>
2021-12-10 18:24:06 -0500

    bug fix

--- Assets/Resources/Apple/Prefab/Apple.cs
+++ Assets/Resources/Apple/Prefab/Apple.cs
@@ -9,50 +9,52 @@ public class Apple : MonoBehaviour
     public float velocity;
     public float birth_time;
     public GameObject birth_turret;
-	public Text textDisplay; 
-	public int doNothing = 0;
-	public GameObject car;
-	//public int num_lives = 5;
-    
+    public Text textDisplay;
+    public int doNothing = 0;
+    public GameObject car;
+    //public int num_lives = 5;
+
     // Start is called before the first frame update
     void Start()
-    {        
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
-		car = GameObject.Find("CarObj");
+        car = GameObject.Find("CarObj");
         if (Time.time - birth_time > 10.0f)  // apples live for 10 sec
         {
             Destroy(transform.gameObject);
         }
         transform.position = transform.position + velocity * direction * Time.deltaTime;
-		
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        ////////////////////////////////////////////////
-        // WRITE CODE HERE:
-        // (a) if the object collides with Claire, subtract one life from her, and destroy the apple
-        // (b) if the object collides with another apple, or its own turret that launched it (birth_turret), don't do anything
-        // (c) if the object collides with anything else (e.g., terrain, a different turret), destroy the apple
-        ////////////////////////////////////////////////
-		if(other.GetComponent<Collider>().name=="CarObj"&& Turret.isAppleHitClaire==true){
-			car.GetComponent<Car>().audio_source.PlayOneShot(car.GetComponent<Car>().carShotByAppleAudio,1.0F);
-			car.GetComponent<Car>().player_health = car.GetComponent<Car>().player_health - 0.1f;
-			// Debug.Log("car has been shooted");
-			// Debug.Log(car.GetComponent<Car>().player_health);
-			Destroy(transform.gameObject);
-			Turret.isAppleHitClaire=false;
-		}else if(other.GetComponent<Collider>().name == "Apple" || other.GetComponent<Collider>().gameObject == birth_turret){
-			doNothing++;
-		}else{
-			//destroy the apple;
-			Destroy(transform.gameObject);
-			
-		}
-       
+        if (Time.time - birth_time > 0.5f)
+        {
+            if (other.GetComponent<Collider>().name == "CarObj" && Turret.isAppleHitClaire == true)
+            {
+                car.GetComponent<Car>().audio_source.PlayOneShot(car.GetComponent<Car>().carShotByAppleAudio, 1.0F);
+                car.GetComponent<Car>().player_health = car.GetComponent<Car>().player_health - 0.1f;
+                // Debug.Log("car has been shooted");
+                // Debug.Log(car.GetComponent<Car>().player_health);
+                Destroy(transform.gameObject);
+                Turret.isAppleHitClaire = false;
+            }
+            else if (other.GetComponent<Collider>().name == "Apple" || other.GetComponent<Collider>().gameObject == birth_turret)
+            {
+                doNothing++;
+            }
+            else
+            {
+                //destroy the apple;
+                Destroy(transform.gameObject);
+
+            }
+        }
+
     }
 }
