84fc0067dfd6d68aa98fad278d75034df05b5d75
jurgen <tigeryhw@gmail.com>
2021-12-10 17:29:35 -0500

    fix animation

--- Assets/Script/girlWaveHands.cs
+++ Assets/Script/girlWaveHands.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 public class girlWaveHands : MonoBehaviour
 {
     // Start is called before the first frame update
-	private Animator animation_controller;
-	public GameObject car;
-	public GameObject destinationHouse;
-	//private Vector3 dist;
+    private Animator animation_controller;
+    public GameObject car;
+    public GameObject destinationHouse;
+
+    private int NextUpdate;
+    //private Vector3 dist;
     void Start()
     {
+		NextUpdate = 0;
         animation_controller = GetComponent<Animator>();
-		car = GameObject.Find("CarObj");
+        car = GameObject.Find("CarObj");
     }
 
     // Update is called once per frame
     void Update()
     {
         float dist = Vector3.Distance(car.transform.position, destinationHouse.transform.position);
-		// Debug.Log(car.GetComponent<Car>().hasDelivered);
-		if(car.GetComponent<Car>().hasDelivered){
-			animation_controller.SetBool("isCongrats", true);
-			
-		}else if(dist<100.0f){
+        // Debug.Log(car.GetComponent<Car>().hasDelivered);
+        if (Time.time > NextUpdate)
+        {
+			animation_controller.SetBool("isCongrats", false);
 			animation_controller.SetBool("isWaving", true);
-		}else{}
-		
+
+        }
+        else if (dist < 100.0f)
+        {
+            animation_controller.SetBool("isWaving", true);
+        }
+
+    }
+
+    public void Congrate()
+    {
+        NextUpdate = Mathf.FloorToInt(Time.time + 5);
+        animation_controller.SetBool("isCongrats", true);
+		animation_controller.SetBool("isWaving", false);
     }
 }
